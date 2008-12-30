e762c95984f00df16c441a3b3ba33a94f8bd1915
Florian Ragwitz <rafl@debian.org>
2008-12-30 04:54:47 +0100

    Properly stringify named params with labels different from the variable name.

--- lib/Parse/Method/Signatures/Param/Named.pm
+++ lib/Parse/Method/Signatures/Param/Named.pm
@@ -13,16 +13,29 @@ has label => (
     lazy_build => 1,
 );
 
-sub _build_label {
+sub _label_from_variable_name {
     my ($self) = @_;
     # strip sigil
     return substr($self->variable_name, 1);
 }
 
+sub _build_label {
+    my ($self) = @_;
+    return $self->_label_from_variable_name;
+}
+
 sub _stringify_variable_name {
     my ($self, $required) = @_;
     my $ret = q{:};
-    $ret .= $self->variable_name;
+    my ($before, $after) = (q{}) x 2;
+
+    if ($self->label ne $self->_label_from_variable_name) {
+        $before = $self->label . q{(};
+        $after  = q{)};
+    }
+
+    $ret .= $before . $self->variable_name . $after;
+
     $ret .= '!' if $required;
     return $ret;
 }
