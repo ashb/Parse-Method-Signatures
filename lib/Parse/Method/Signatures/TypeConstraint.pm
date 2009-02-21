afb6c5671646f5b3bee5309fea31f098df0f48c0
Florian Ragwitz <rafl@debian.org>
2009-02-21 15:38:29 +0100

    Implement building of real meta type constraints from our tc class.

--- lib/Parse/Method/Signatures/TypeConstraint.pm
+++ lib/Parse/Method/Signatures/TypeConstraint.pm
@@ -1,7 +1,8 @@
 package Parse::Method::Signatures::TypeConstraint;
 
 use Moose;
-use MooseX::Types::Moose qw/Str HashRef/;
+use MooseX::Types::Moose qw/Str HashRef CodeRef/;
+use Parse::Method::Signatures::Types qw/TypeConstraint/;
 
 use namespace::clean -except => 'meta';
 
@@ -17,6 +18,56 @@ has data => (
   isa      => Str|HashRef,
   required => 1,
 );
+
+has tc => (
+    is => 'ro',
+    isa => TypeConstraint,
+    lazy => 1,
+    builder => '_build_tc',
+);
+
+has tc_callback => (
+    is       => 'ro',
+    isa      => CodeRef,
+    default  => sub { \&find_registered_constraint },
+);
+
+sub _build_tc {
+    my ($self) = @_;
+    return $self->visit($self->data);
+}
+
+sub visit {
+    my ($self, $data) = @_;
+    unless (ref $data) {
+        return $self->invoke_callback($data);
+    }
+    elsif (exists $data->{-or}) {
+        my @types = map { $self->visit($_) } @{ $data->{-or} };
+        return @types if scalar @types == 1;
+        return Moose::Meta::TypeConstraint::Union->new(type_constraints => \@types);
+    }
+    elsif (exists $data->{-type}) {
+        my @params = map { $self->visit($_) } @{ $data->{-params} };
+        my $type = $self->invoke_callback($data->{-type});
+        return $type->parameterize(@params);
+    }
+    else {
+        confess 'failed to visit tc';
+    }
+}
+
+sub invoke_callback {
+    my $self = shift;
+    $self->tc_callback->($self, @_);
+}
+
+sub find_registered_constraint {
+    my ($self, $name) = @_;
+    warn $name;
+    my $registry = Moose::Util::TypeConstraints->get_type_constraint_registry;
+    return $registry->find_type_constraint($name) || $name;
+}
 
 __PACKAGE__->meta->make_immutable;
 
